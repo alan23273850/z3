820b7393a5d519cdb0b5de977061bf374d4bfe45
Yu-Fang Chen <gulu0724@gmail.com>
2021-06-30 15:44:03 +0800

    add summary to run_benchmarks.py

--- benchmarks/run_benchmarks.py
+++ benchmarks/run_benchmarks.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import os, sys
 from to_be_imported import from_file_to_data_row
+import subprocess
 # os.system('mkdir statistics')
 
 with open(f"statistics/{sys.argv[1]}.csv", 'w', buffering=1) as f:
@@ -13,3 +14,18 @@ with open(f"statistics/{sys.argv[1]}.csv", 'w', buffering=1) as f:
                 result_list = from_file_to_data_row(file)
                 print(','.join(result_list))
                 print(','.join(result_list), file=f)
+
+incor_sat = int(subprocess.check_output(f"grep _unsat statistics/{sys.argv[1]}.csv| grep ,sat|wc -l", shell=True).decode("utf8").split()[0])
+print ('Incorrect SAT: ', incor_sat)
+incor_unsat = int(subprocess.check_output(f"grep _sat statistics/{sys.argv[1]}.csv| grep ,unsat|wc -l", shell=True).decode("utf8").split()[0])
+print ('Incorrect UNSAT: ', incor_unsat)
+sat = int(subprocess.check_output(f"grep ,sat statistics/{sys.argv[1]}.csv|wc -l", shell=True).decode("utf8").split()[0])
+print ('SAT: ', sat)
+unsat = int(subprocess.check_output(f"grep ,unsat statistics/{sys.argv[1]}.csv|wc -l", shell=True).decode("utf8").split()[0])
+print ('UNSAT: ', unsat)
+unknown = int(subprocess.check_output(f"grep ,unknown statistics/{sys.argv[1]}.csv|wc -l", shell=True).decode("utf8").split()[0])
+print ('UNKNOWN: ', unknown)
+timeout = int(subprocess.check_output(f"grep ,timeout statistics/{sys.argv[1]}.csv|wc -l", shell=True).decode("utf8").split()[0])
+print ('TIMEOUT: ', timeout)
+print('TOTAL: ', sat+unsat+timeout+unknown)
+
