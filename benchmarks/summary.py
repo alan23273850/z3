4a453f1d65e5465cd8c214c89d42f22e79e872f2
Wei-Lun Tsai <alan23273850@gmail.com>
2021-07-08 17:00:32 +0800

    add csv and fix summary.py

--- benchmarks/summary.py
+++ benchmarks/summary.py
@@ -4,19 +4,24 @@ from to_be_imported import from_file_to_data_row
 import re,subprocess
 # os.system('mkdir statistics')
 
-incor_sat = int(subprocess.check_output(f"grep _unsat statistics/{sys.argv[1]}.csv| grep ,sat|wc -l", shell=True).decode("utf8").split()[0])
+if sys.argv[1].endswith('.csv'):
+    filename = sys.argv[1]
+else:
+    filename = f'statistics/{sys.argv[1]}.csv'
+
+incor_sat = int(subprocess.check_output(f"grep _unsat {filename}| grep ,sat|wc -l", shell=True).decode("utf8").split()[0])
 print ('Incorrect SAT: ', incor_sat)
-incor_unsat = int(subprocess.check_output(f"grep _sat statistics/{sys.argv[1]}.csv| grep ,unsat|wc -l", shell=True).decode("utf8").split()[0])
+incor_unsat = int(subprocess.check_output(f"grep _sat {filename}| grep ,unsat|wc -l", shell=True).decode("utf8").split()[0])
 print ('Incorrect UNSAT: ', incor_unsat)
-SAT = int(subprocess.check_output(f"grep _sat statistics/{sys.argv[1]}.csv|wc -l", shell=True).decode("utf8").split()[0])
-sat = int(subprocess.check_output(f"grep ,sat statistics/{sys.argv[1]}.csv|wc -l", shell=True).decode("utf8").split()[0])
+SAT = int(subprocess.check_output(f"grep _sat {filename}|wc -l", shell=True).decode("utf8").split()[0])
+sat = int(subprocess.check_output(f"grep ,sat {filename}|wc -l", shell=True).decode("utf8").split()[0])
 print ('SAT: ', sat, '/', SAT)
-UNSAT = int(subprocess.check_output(f"grep _unsat statistics/{sys.argv[1]}.csv|wc -l", shell=True).decode("utf8").split()[0])
-unsat = int(subprocess.check_output(f"grep ,unsat statistics/{sys.argv[1]}.csv|wc -l", shell=True).decode("utf8").split()[0])
+UNSAT = int(subprocess.check_output(f"grep _unsat {filename}|wc -l", shell=True).decode("utf8").split()[0])
+unsat = int(subprocess.check_output(f"grep ,unsat {filename}|wc -l", shell=True).decode("utf8").split()[0])
 print ('UNSAT: ', unsat, '/', UNSAT)
-unknown = int(subprocess.check_output(f"grep ,unknown statistics/{sys.argv[1]}.csv|wc -l", shell=True).decode("utf8").split()[0])
+unknown = int(subprocess.check_output(f"grep ,unknown {filename}|wc -l", shell=True).decode("utf8").split()[0])
 print ('UNKNOWN: ', unknown)
-timeout = int(subprocess.check_output(f"grep ,timeout statistics/{sys.argv[1]}.csv|wc -l", shell=True).decode("utf8").split()[0])
+timeout = int(subprocess.check_output(f"grep ,timeout {filename}|wc -l", shell=True).decode("utf8").split()[0])
 print ('TIMEOUT: ', timeout)
 
 print('TOTAL: ', sat+unsat+timeout+unknown)
