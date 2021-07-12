147dc8e9b7a7ffd074db04dfe901ee3475254d4b
Wei-Lun Tsai <alan23273850@gmail.com>
2021-07-12 17:06:05 +0800

    add b27b8024022c8573502ff2fe619261542013a20c_small.csv

--- benchmarks/run_benchmarks.py
+++ benchmarks/run_benchmarks.py
@@ -4,7 +4,7 @@ from to_be_imported import from_file_to_data_row
 import subprocess
 # os.system('mkdir statistics')
 
-with open(f"statistics/{sys.argv[1]}.csv", 'w', buffering=1) as f:
+with open(f"statistics/{sys.argv[1]}_small.csv", 'w', buffering=1) as f:
     # print('filename,z3-time,z3-res,cvc4-time,cvc4-res,trau-time,trau-res,trau-msg,final', file=f)
     print('filename,z3-time,z3-res,message', file=f)
     for dirPath, dirNames, fileNames in list(os.walk('debug')) + list(os.walk('handmade')) + list(os.walk('sample_50')):
@@ -15,4 +15,4 @@ with open(f"statistics/{sys.argv[1]}.csv", 'w', buffering=1) as f:
                 print(','.join(result_list))
                 print(','.join(result_list), file=f)
 
-os.system(f'python3 summary.py {sys.argv[1]}')
+os.system(f'python3 summary.py {sys.argv[1]}_small.csv')
