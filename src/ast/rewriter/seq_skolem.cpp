04e1beda8d2af02af40f35c0c260571e562cd803
Wei-Lun Tsai <alan23273850@gmail.com>
2021-07-16 19:15:26 +0800

    fix all (except mk_skolem part)

--- src/ast/rewriter/seq_skolem.cpp
+++ src/ast/rewriter/seq_skolem.cpp
@@ -206,30 +206,30 @@ expr_ref skolem::mk_digit2bv(expr* ch, sort* bv_sort) {
 }
 
 expr_ref skolem::mk_parikh_image_counter(expr *var, expr *ch) {
-    return expr_ref(seq.mk_skolem(symbol("parikh image counter"), 2,
+    return expr_ref(seq.mk_skolem(symbol("seq.parikh_image_counter"), 2,
         std::initializer_list<expr*>({var, ch}).begin(),
         m.mk_sort(a.get_family_id(), INT_SORT)), m);
 }
 
 expr_ref skolem::mk_FA_self_loop_char(expr *var, unsigned i) {
-    return expr_ref(seq.mk_skolem(symbol("FA self loop char"), 2,
+    return expr_ref(seq.mk_skolem(symbol("seq.fa_self_loop_char"), 2,
         std::initializer_list<expr*>({var, a.mk_int(i)}).begin(),
         m.mk_sort(a.get_family_id(), CHAR_SORT)), m);
 }
 
 expr_ref skolem::mk_FA_self_loop_counter(expr *e) {
-    return expr_ref(seq.mk_skolem(symbol("FA self loop counter"), 1, &e,
+    return expr_ref(seq.mk_skolem(symbol("seq.fa_self_loop_counter"), 1, &e,
         m.mk_sort(a.get_family_id(), INT_SORT)), m);
 }
 
 expr_ref skolem::mk_PFA_loop_counter(unsigned eqid, unsigned i, unsigned j) {
-    return expr_ref(seq.mk_skolem(symbol("PFA loop counter"), 3,
+    return expr_ref(seq.mk_skolem(symbol("seq.pfa_loop_counter"), 3,
         std::initializer_list<expr*>({a.mk_int(eqid), a.mk_int(i), a.mk_int(j)}).begin(),
         m.mk_sort(a.get_family_id(), INT_SORT)), m);
 }
 
 expr_ref skolem::mk_PFA_edge_selection(unsigned eqid, const std::pair<int, int> &state1, const std::pair<int, int> &state2) {
-    return expr_ref(seq.mk_skolem(symbol("PFA edge selection"), 5,
+    return expr_ref(seq.mk_skolem(symbol("seq.pfa_edge_selection"), 5,
         std::initializer_list<expr*>({a.mk_int(eqid), a.mk_int(state1.first), a.mk_int(state1.second), a.mk_int(state2.first), a.mk_int(state2.second)}).begin(),
         m.mk_sort(a.get_family_id(), BOOL_SORT)), m);
 }
